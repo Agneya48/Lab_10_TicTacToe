7c4d5b60f65069eb2654441ed6c2aa26c1d8ec55
Agneya48 <josh.t.hampton@gmail.com>
2024-11-08 01:41:36 -0500

    Initial Commit

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,12 @@
-public class Main {
+public class TicTacToe {
+
+    //start with the creating the array for the board
+    private static final int ROWS = 3;
+    private static final int COLS = 3;
+    private static String board [][] = new String[ROWS][COLS];
+
+    //now the main process that runs the game
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
